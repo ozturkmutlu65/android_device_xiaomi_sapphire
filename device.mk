039f313ffa5c5a200ceeeac16833b37837b6c10c
Mutlu Öztürk <ozturkmutlu65@gmail.com>
2026-04-20 11:47:27 +0300

    sapphire: Add GoogleCamera configuration to device.mk
    Signed-off-by: Mutlu Öztürk <ozturkmutlu65@gmail.com>

--- device.mk
+++ device.mk
@@ -276,6 +276,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2022-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2022-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# GoogleCamera
+$(call inherit-product-if-exists, vendor/xiaomi/GoogleCamera/config.mk)
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
