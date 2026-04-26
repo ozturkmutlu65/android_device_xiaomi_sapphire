e64c0fbc61bc4bdabc511beb8aefca5ca36eb2f2
ozturkmutlu65 <ozturkmutlu65@gmail.com>
2026-04-26 07:55:24 +0300

    sapphire: Migrate QTI power HAL to soong_config_set

--- device.mk
+++ device.mk
@@ -469,6 +469,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    
+$(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sapphire)
+$(call soong_config_set,qtipower,tap_to_wake_node,/proc/tp_gesture)
 
 # QCC
 PRODUCT_PACKAGES += \
